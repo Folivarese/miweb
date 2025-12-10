9ddb6a07f334b7160a9228e1a89b677675b379ab
Fabian Ezequiel Olivares <161616681+Folivarese@users.noreply.github.com>
2025-12-10 11:04:46 -0300

    Add files via upload

---
+++ efecto.js
@@ -0,0 +1,9 @@
+// Activa el parpadeo rojo de la tarjeta
+function activarAlertaReto() {
+    document.body.classList.add("alerta-reto");
+}
+
+// Detiene el parpadeo rojo de la tarjeta
+function desactivarAlertaReto() {
+    document.body.classList.remove("alerta-reto");
+}
