88effbee225a7129534d6880d53757ba416048bd
Fabian Ezequiel Olivares <161616681+Folivarese@users.noreply.github.com>
2025-12-10 14:39:00 -0300

    Add files via upload

--- efecto.js
+++ efecto.js
@@ -1,9 +1,11 @@
 // Activa el parpadeo rojo de la tarjeta
 function activarAlertaReto() {
+    // Solo añade la clase al BODY.
     document.body.classList.add("alerta-reto");
 }
 
 // Detiene el parpadeo rojo de la tarjeta
 function desactivarAlertaReto() {
+    // Solo quita la clase del BODY.
     document.body.classList.remove("alerta-reto");
 }
